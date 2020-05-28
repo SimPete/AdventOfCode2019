3d4694576e09fb6b165598dd29bcddd3b21299f1
unknown <cote.simon.pierre@gmail.com>
2020-05-28 09:42:42 -0400

    Work continued on Day3. Travelling function done. Need to calculate manhattan distance.

--- Day3_WiresCrossing.py
+++ Day3_WiresCrossing.py
@@ -6,6 +6,10 @@ from lib import Day3_fct
 ### CREATING THE GRID
 nb_lines = 40
 nb_columns = 40
+crossingFound = 0
+wire1Symbol=5
+wire2Symbol=8
+wireCrossingSymbol=7
 GRID = numpy.zeros((nb_lines,nb_columns),dtype = int)
 
 # print('\n'.join([''.join(['{:2}'.format(item) for item in row]) 
@@ -15,9 +19,10 @@ GRID = numpy.zeros((nb_lines,nb_columns),dtype = int)
 center_of_lines = int(nb_lines/2)
 center_of_columns = int(nb_columns/2)
 GRID[center_of_lines,center_of_columns] = 1
-current_position=(center_of_lines, center_of_columns)
+startingPosition=(center_of_lines, center_of_columns)
+crossing=[]
 
-print(current_position)
+print("Current position is : ", startingPosition)
 
 ### IMPORT THE WIRES DATA
 input = open('./ressources/Day3_Test1.txt','r')
@@ -28,14 +33,84 @@ print(Wire1)
 print(Wire2)
 
 ### DRAW THE FIRST WIRE
-for i in range((len(Wire1))):  
-    Dir=Wire1[i][0] #Direction
-    Steps=Wire1[i][1] #Number of steps
-    print(Dir, Steps)
+currentPosition=startingPosition #Reset currentPosition to center
+for command in range((len(Wire1))): 
+    dir=Wire1[command][0] #direction
+    steps=Wire1[command][1] #Number of steps
+    print(dir, steps)
     
-    # Walk the steps in the direction
-    for s in range(int(Steps)):
-        GRID[int(nb_lines/2),int(nb_columns/2)] = 1
+    if dir == "U":
+        for step in range(int(steps)):
+            currentPosition=(currentPosition[0]-1, currentPosition[1])
+            GRID[currentPosition] = wire1Symbol
+    elif dir == "D":
+        for step in range(int(steps)):
+            currentPosition=(currentPosition[0]+1, currentPosition[1])
+            GRID[currentPosition] = wire1Symbol
+    elif dir == "L":
+        for step in range(int(steps)):
+            currentPosition=(currentPosition[0], currentPosition[1]-1)
+            GRID[currentPosition] = wire1Symbol
+    elif dir  == "R":
+        for step in range(int(steps)):
+            currentPosition=(currentPosition[0], currentPosition[1]+1)
+            GRID[currentPosition] = wire1Symbol
+    else :
+        print("FATAL ERROR: Unsupported direction received")
+
+### DRAW THE SECOND WIRE
+currentPosition=startingPosition #Reset currentPosition to center
+for command in range((len(Wire2))): 
+    dir=Wire2[command][0] #direction
+    steps=Wire2[command][1] #Number of steps
+    print(dir, steps)
+    
+    if dir == "U":
+        for step in range(int(steps)):
+            currentPosition=(currentPosition[0]-1, currentPosition[1])
+            if GRID[currentPosition] == wire1Symbol:
+                crossingFound+=1
+                print("CROSSING FOUND! Crossing found count is now : ", crossingFound)
+                crossing.append(currentPosition)
+                GRID[currentPosition] = wireCrossingSymbol
+            else:
+                GRID[currentPosition] = wire2Symbol
+    elif dir == "D":
+        for step in range(int(steps)):
+            currentPosition=(currentPosition[0]+1, currentPosition[1])
+            if GRID[currentPosition] == wire1Symbol:
+                crossingFound+=1
+                print("CROSSING FOUND! Crossing found count is now : ", crossingFound)
+                crossing.append(currentPosition)
+                GRID[currentPosition] = wireCrossingSymbol
+            else:
+                GRID[currentPosition] = wire2Symbol
+    elif dir == "L":
+        for step in range(int(steps)):
+            currentPosition=(currentPosition[0], currentPosition[1]-1)
+            if GRID[currentPosition] == wire1Symbol:
+                crossingFound+=1
+                print("CROSSING FOUND! Crossing found count is now : ", crossingFound)
+                crossing.append(currentPosition)
+                GRID[currentPosition] = wireCrossingSymbol
+            else:
+                GRID[currentPosition] = wire2Symbol
+    elif dir  == "R":
+        for step in range(int(steps)):
+            currentPosition=(currentPosition[0], currentPosition[1]+1)
+            if GRID[currentPosition] == wire1Symbol:
+                crossingFound+=1
+                print("CROSSING FOUND! Crossing found count is now : ", crossingFound)
+                crossing.append(currentPosition)
+                GRID[currentPosition] = wireCrossingSymbol
+            else:
+                GRID[currentPosition] = wire2Symbol
+    else :
+        print("FATAL ERROR: Unsupported direction received")
+        
+Day3_fct.gridprint(GRID)
+print("Crossing points found are : ", crossing)
 
 
-# Day3_fct.gridprint(GRID)
+
+
--- lib/Day3_fct.py
+++ lib/Day3_fct.py
@@ -1,3 +1,10 @@
+def travel(Dir, Steps, Current_Position):
+    for s in range(int(Steps)):
+        GRID[int(nb_lines/2),int(nb_columns/2)] = 1
+
+
+    return Current_Position
+    
 def gridprint(GRID):
     print('\n'.join([''.join(['{:2}'.format(item) for item in row]) 
         for row in GRID]))
